041cf60f092633d111ae7cd567f6efbaa37126b2
bhuvanesh-mani <bhuvaneshmani@virtusa.com>
2019-12-20 00:31:56 +0000

    Updated AddUser

--- src/main/java/test/AddUser.java
+++ src/main/java/test/AddUser.java
@@ -1,7 +1,12 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddUser {
 	@FindBy(name="login")
@@ -10,10 +15,19 @@ public class AddUser {
 	private WebElement password;
 	@FindBy(name="click")
 	private WebElement submit;
-		
+	@FindBy(name="test")
+	private WebElement confirmation;
+	private WebDriverWait wait;
+	
+	
+	public AddUser(WebDriver driver) {
+		this.wait = new WebDriverWait(driver, 10);
+	}
+	
 	public void addUser(String user, String pass) {
 		userName.sendKeys(user);
 		password.sendKeys(pass);
 		submit.click();
+		wait.until(ExpectedConditions.visibilityOf(confirmation));
 	}
 }
